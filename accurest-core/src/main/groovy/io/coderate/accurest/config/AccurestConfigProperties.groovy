ed7e7e1a30b470b5221a1d87bc09d99bc56d58ba
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-02-21 21:15:42 +0100

    Added javadocs for extension parameters

--- accurest-core/src/main/groovy/io/coderate/accurest/config/AccurestConfigProperties.groovy
+++ accurest-core/src/main/groovy/io/coderate/accurest/config/AccurestConfigProperties.groovy
@@ -1,5 +1,4 @@
 package io.coderate.accurest.config
-
 /**
  * @author Jakub Kubrynski
  */
@@ -12,7 +11,20 @@ class AccurestConfigProperties {
 	List<String> ignoredFiles = []
 	String[] imports = []
 	String[] staticImports = []
+
+	/**
+	 * Directory containing contracts written using the GroovyDSL
+	 */
 	File contractsDslDir
+
+	/**
+	 * Test source directory where tests generated from Groovy DSL should be placed
+	 */
 	File generatedTestSourcesDir
+
+	/**
+	 * Dir where the generated Wiremock stubs from Groovy DSL should be placed.
+	 * You can then mention them in your packaging task to create jar with stubs
+	 */
 	File stubsOutputDir
 }
