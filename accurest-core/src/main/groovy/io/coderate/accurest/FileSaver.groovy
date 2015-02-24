cbf82e7d73523320aa984b0e63bf2eb4be8576c0
Marcin Zajaczkowski <marcin.zajaczkowski@4finance.com>
2015-02-24 12:34:03 +0100

    Add logging what class files were generated

--- accurest-core/src/main/groovy/io/coderate/accurest/FileSaver.groovy
+++ accurest-core/src/main/groovy/io/coderate/accurest/FileSaver.groovy
@@ -1,16 +1,19 @@
 package io.coderate.accurest
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import io.coderate.accurest.config.TestFramework
 import io.coderate.accurest.util.NamesUtil
 
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
 
 import static io.coderate.accurest.util.NamesUtil.capitalize
 
 @CompileStatic
+@Slf4j
 class FileSaver {
 
 	File targetDirectory
@@ -23,10 +26,11 @@ class FileSaver {
 
 	void saveClassFile(String fileName, String packageName, byte[] classBytes) {
 
-		def testBaseDir = Paths.get(targetDirectory.absolutePath, NamesUtil.packageToDirectory(packageName))
+		Path testBaseDir = Paths.get(targetDirectory.absolutePath, NamesUtil.packageToDirectory(packageName))
 		Files.createDirectories(testBaseDir)
-		def classPath = Paths.get(testBaseDir.toString(), capitalize(fileName) + framework.classNameSuffix + framework.classExtension)
+		Path classPath = Paths.get(testBaseDir.toString(), capitalize(fileName) + framework.classNameSuffix + framework.classExtension)
 				.toAbsolutePath()
+		log.info("Creating new class file [$classPath]")
 		Files.write(classPath, classBytes, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
 	}
 
