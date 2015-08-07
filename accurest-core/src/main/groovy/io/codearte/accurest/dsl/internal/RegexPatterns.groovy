e0436ace9fd62744b12b17994dc51eb9e455537d
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-08-07 21:38:54 +0200

    [#115] added possibility to have some predefined regex

---
+++ accurest-core/src/main/groovy/io/codearte/accurest/dsl/internal/RegexPatterns.groovy
@@ -0,0 +1,30 @@
+package io.codearte.accurest.dsl.internal
+
+import groovy.transform.CompileStatic
+
+import java.util.regex.Pattern
+
+@CompileStatic
+class RegexPatterns {
+
+	private static final Pattern IP_ADDRESS = Pattern.compile('([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])');
+	private static final Pattern HOSTNAME_PATTERN = Pattern.compile('((http[s]?|ftp):\\/)\\/?([^:\\/\\s]+)(:[0-9]{1,5})?');
+	private static final Pattern EMAIL = Pattern.compile('[_]*([a-z0-9]+(\\.|_*)?)+@([a-z][a-z0-9-]+(\\.|-*\\.))+[a-z]{2,6}');
+	private static final Pattern URL = Pattern.compile('((www\\.|(http|https|ftp|news|file)+\\:\\/\\/)[_.a-z0-9-]+\\.[a-z0-9\\/_:@=.+?,##%&~-]*[^.|\\\'|\\# |!|\\(|?|,| |>|<|;|\\)])');
+
+	String ipAddress() {
+		return IP_ADDRESS.pattern()
+	}
+
+	String hostname() {
+		return HOSTNAME_PATTERN.pattern()
+	}
+
+	String email() {
+		return EMAIL.pattern()
+	}
+
+	String url() {
+		return URL.pattern()
+	}
+}
