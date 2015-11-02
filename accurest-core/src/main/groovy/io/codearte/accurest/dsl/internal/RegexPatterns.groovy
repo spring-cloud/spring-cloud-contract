7a852e1e33b9cc6c79463095958edc8af0ef6a48
Adam Wojszczyk <adam.wojszczyk@4finance.com>
2015-11-02 11:37:14 +0100

    Add regex to match numbers (integers and decimals)

--- accurest-core/src/main/groovy/io/codearte/accurest/dsl/internal/RegexPatterns.groovy
+++ accurest-core/src/main/groovy/io/codearte/accurest/dsl/internal/RegexPatterns.groovy
@@ -9,10 +9,11 @@ class RegexPatterns {
 
 	private static final Pattern TRUE_OR_FALSE = Pattern.compile(/(true|false)/)
 	private static final Pattern ONLY_ALPHA_UNICODE = Pattern.compile(/[\p{L}]*/)
+	private static final Pattern NUMBER = Pattern.compile('-?\\d*(\\.\\d+)?')
 	private static final Pattern IP_ADDRESS = Pattern.compile('([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])');
 	private static final Pattern HOSTNAME_PATTERN = Pattern.compile('((http[s]?|ftp):\\/)\\/?([^:\\/\\s]+)(:[0-9]{1,5})?');
 	private static final Pattern EMAIL = Pattern.compile('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}');
-	private static final Pattern URL = Pattern.compile('((www\\.|(http|https|ftp|news|file)+\\:\\/\\/)[_.a-z0-9-]+\\.[a-z0-9\\/_:@=.+?,##%&~-]*[^.|\\\'|\\# |!|\\(|?|,| |>|<|;|\\)])');
+	private static final Pattern URL = Pattern.compile('((www\\.|(http|https|ftp|news|file)+\\:\\/\\/)[_.a-z0-9-]+\\.[a-z0-9\\/_:@=.+?,##%&~-]*[^.|\\\'|\\# |!|\\(|?|,| |>|<|;|\\)])')
 
 
 	String onlyAlphaUnicode() {
@@ -37,5 +38,9 @@ class RegexPatterns {
 
 	String url() {
 		return URL.pattern()
+	}
+
+	String number() {
+		return NUMBER.pattern()
 	}
 }
