04721e13b14c5eaf69537a4ef78ab343ab2eaf1b
mzielinski <zielinskimaciej@yahoo.com>
2015-11-03 23:07:26 +0100

    #166 directory structure of dsl groovy scripts should be used in generated specifications

--- accurest-core/src/main/groovy/io/codearte/accurest/TestGenerator.groovy
+++ accurest-core/src/main/groovy/io/codearte/accurest/TestGenerator.groovy
@@ -7,6 +7,9 @@ import org.codehaus.plexus.util.DirectoryScanner
 import java.util.concurrent.atomic.AtomicInteger
 
 import static io.codearte.accurest.util.NamesUtil.afterLast
+import static io.codearte.accurest.util.NamesUtil.beforeLast
+import static io.codearte.accurest.util.NamesUtil.directoryToPackage
+
 /**
  * @author Jakub Kubrynski
  */
@@ -62,14 +65,20 @@ class TestGenerator {
 			}
 			if (filesToClass.size()) {
 				def className = afterLast(includedDirectoryRelativePath, File.separator) + configProperties.targetFramework.classNameSuffix
-				def classBytes = generator.buildClass(filesToClass, className, packageNameForClass).bytes
-				saver.saveClassFile(className, packageNameForClass, classBytes)
+				def packageName = buildPackage(packageNameForClass, includedDirectoryRelativePath)
+				def classBytes = generator.buildClass(filesToClass, className, packageName).bytes
+				saver.saveClassFile(className, packageName, classBytes)
 				counter.incrementAndGet()
 			}
 		}
 	}
-    
-    private String normalizePath(String path) {
+
+    private static String buildPackage(final String packageNameForClass, final String includedDirectoryRelativePath) {
+        String directory = beforeLast(includedDirectoryRelativePath, File.separator)
+        return "$packageNameForClass.${directoryToPackage(directory)}"
+    }
+
+    private static String normalizePath(String path) {
         return FilenameUtils.separatorsToUnix(path)
     }
 }
