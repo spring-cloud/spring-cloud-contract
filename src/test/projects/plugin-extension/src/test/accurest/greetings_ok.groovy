fa27cce1c788870a6ce24ab4548981bedbb3b1a7
Mariusz Smykula <mariuszs@gmail.com>
2016-04-30 11:56:12 +0200

    Revert "simplified config"
    [ci skip]
    
    This reverts commit 12173325d38eca1bc5597e518e2eb9fe44d73d06.

---
+++ src/test/projects/plugin-extension/src/test/accurest/greetings_ok.groovy
@@ -0,0 +1,21 @@
+io.codearte.accurest.dsl.GroovyDsl.make {
+    priority 2
+    request {
+        method 'GET'
+        urlPath('/greeting') {
+            queryParameters {
+                parameter 'name': 'Something'
+            }
+        }
+        headers {
+            header 'Content-Type': 'application/json'
+        }
+    }
+    response {
+        status 200
+        body(
+                id: 1,
+                content: "Hello, Something!"
+        )
+    }
+}
