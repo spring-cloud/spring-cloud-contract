26eb6f72a792f388f0e1f1bd171d25828acb2955
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-07-10 12:34:04 +0200

    Updated scripts

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -5,14 +5,15 @@ set -o errexit
 ROOT_FOLDER=`pwd`
 echo "Current folder is $ROOT_FOLDER"
 
-mkdir -p build
+mkdir -p target
 CONTRACT_VERIFIER_VERSION=1.0.0.BUILD-SNAPSHOT
 export CONTRACT_VERIFIER_VERSION=${CONTRACT_VERIFIER_VERSION}
 
 echo "Current Spring Cloud Contract Verifier version is ${CONTRACT_VERIFIER_VERSION}"
 
-cd build
+cd target
 echo "Cloning samples"
+rm -rf spring-cloud-contract-verifier-samples
 git clone https://github.com/spring-cloud-samples/spring-cloud-contract-verifier-samples
 cd spring-cloud-contract-verifier-samples
 
