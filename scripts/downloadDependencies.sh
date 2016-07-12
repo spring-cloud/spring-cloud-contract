c76de73f3f927d0a5834b984ae08c8dc896e1529
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-07-12 13:25:14 +0200

    Fixed CircleCI caching

---
+++ scripts/downloadDependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+ROOT_FOLDER=`pwd`
+echo "Current folder is $ROOT_FOLDER"
+
+if [[ ! -e "${ROOT_FOLDER}/.git" ]]; then
+    cd ..
+    ROOT_FOLDER=`pwd`
+fi
+
+./mvnw -s .settings.xml --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/spring-cloud-contact-verifier-gradle-plugin && ./gradlew resolveDependencies || true
+cd $ROOT_FOLDER/spring-cloud-contract-verifier-standalone-test-samples && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/spring-cloud-contract-verifier-standalone-test-samples && ./downloadGradleDeps.sh
---
+++ spring-cloud-contract-verifier-standalone-test-samples/downloadGradleDeps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit
+
+LOCAL_MAVEN_REPO=${LOCAL_MAVEN_REPO-~/.m2}
+VERIFIER_VERSION=${VERIFIER_VERSION:-1.0.0.BUILD-SNAPSHOT}
+ROOT=`pwd`
+
+cat <<EOF
+Downloading deps
+LOCAL_MAVEN_REPO=${LOCAL_MAVEN_REPO}
+VERIFIER_VERSION=${VERIFIER_VERSION}
+EOF
+
+cd http-server
+./gradlew resolveDependencies -PverifierVersion=${VERIFIER_VERSION}
+cd $ROOT
+cd http-client
+./gradlew resolveDependencies -PverifierVersion=${VERIFIER_VERSION} --stacktrace
+cd $ROOT
+
+cd contract-verifier-sample-stream-source
+./gradlew resolveDependencies -PverifierVersion=${VERIFIER_VERSION} --stacktrace
+cd $ROOT
+cd contract-verifier-sample-stream-sink
+./gradlew resolveDependencies -PverifierVersion=${VERIFIER_VERSION} --stacktrace
+cd $ROOT
