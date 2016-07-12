dd1410b5f975b166cf75e24e65a6ef593b78ca0b
Dave Syer <dsyer@pivotal.io>
2016-07-12 10:45:03 +0100

    Convert samples to Java

---
+++ samples/samples-messaging-integration/src/main/java/org/springframework/cloud/contract/verifier/samples/messaging/BookListener.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 2013-2016 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.springframework.cloud.contract.verifier.samples.messaging;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+public class BookListener {
+
+	private static final Logger log = LoggerFactory.getLogger(BookListener.class);
+
+	/**
+	 * Scenario for "should generate tests triggered by a message": client side: if sends
+	 * a message to input.messageFrom then message will be sent to output.messageFrom
+	 * server side: will send a message to input, verify the message contents and await
+	 * upon receiving message on the output messageFrom
+	 */
+	public Message<BookReturned> returnBook(BookReturned bookReturned) {
+		log.info("Returning book [$bookReturned]");
+		return MessageBuilder.withPayload(bookReturned)
+				.setHeader("BOOK-NAME", bookReturned.bookName).build();
+	}
+
+	/**
+	 * Scenario for "should generate tests triggered by a message": client side: if sends
+	 * a message to input.messageFrom then message will be sent to output.messageFrom
+	 * server side: will send a message to input, verify the message contents and await
+	 * upon receiving message on the output messageFrom
+	 */
+	public void bookDeleted(BookDeleted bookDeleted) {
+		log.info("Deleting book [$bookDeleted]");
+		bookSuccessfulyDeleted.set(true);
+	}
+
+	private AtomicBoolean bookSuccessfulyDeleted = new AtomicBoolean(false);
+}
