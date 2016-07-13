e9055ba69041c0d719a967947fbe71eb5cad2668
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-07-13 12:19:37 +0200

    Updated the docs

--- spring-cloud-contract-verifier-maven-plugin/publishDocs.sh
+++ spring-cloud-contract-verifier-maven-plugin/publishDocs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn clean site site:stage scm-publish:publish-scm -DskipTests -pl spring-cloud-contract-verifier-maven-plugin
+mvn clean site site:stage scm-publish:publish-scm -DskipTests
