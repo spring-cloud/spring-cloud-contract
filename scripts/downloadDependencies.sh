6d979dd3db2f4623d9a193b5d96faf44005b270e
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-08-12 14:43:14 +0200

    Updating circlei build

--- scripts/downloadDependencies.sh
+++ scripts/downloadDependencies.sh
@@ -5,16 +5,16 @@ source common.sh || source scripts/common.sh || echo "No common.sh script found.
 set -e
 
 ./mvnw -s .settings.xml --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/spring-cloud-contract-tools/spring-cloud-contact-gradle-plugin; ./gradlew resolveDependencies || true
-cd $ROOT_FOLDER/samples/standalone/dsl/http-client; ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/dsl/http-client; ./gradlew resolveDependencies || true
-cd $ROOT_FOLDER/samples/standalone/dsl/http-server; ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/dsl/http-server; ./gradlew resolveDependencies || true
-cd $ROOT_FOLDER/samples/standalone/restdocs/http-client; ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/restdocs/http-client; ./gradlew resolveDependencies || true
-cd $ROOT_FOLDER/samples/standalone/restdocs/http-server; ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/restdocs/http-server; ./gradlew resolveDependencies || true
-cd $ROOT_FOLDER/samples/standalone/messaging/stream-sink; ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/messaging/stream-sink; ./gradlew resolveDependencies || true
-cd $ROOT_FOLDER/samples/standalone/messaging/stream-source; ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/messaging/stream-source; ./gradlew resolveDependencies || true
+cd $ROOT_FOLDER/spring-cloud-contract-tools/spring-cloud-contact-gradle-plugin && ./gradlew resolveDependencies || true
+cd $ROOT_FOLDER/samples/standalone/dsl/http-client && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/dsl/http-client && ./gradlew resolveDependencies || true
+cd $ROOT_FOLDER/samples/standalone/dsl/http-server && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/dsl/http-server && ./gradlew resolveDependencies || true
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-client && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-client && ./gradlew resolveDependencies || true
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-server && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-server && ./gradlew resolveDependencies || true
+cd $ROOT_FOLDER/samples/standalone/messaging/stream-sink && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/messaging/stream-sink && ./gradlew resolveDependencies || true
+cd $ROOT_FOLDER/samples/standalone/messaging/stream-source && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/messaging/stream-source && ./gradlew resolveDependencies || true
