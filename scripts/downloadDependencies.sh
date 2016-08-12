889f35b9952ca1345be4019dd70cbf117bdc660b
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-08-12 14:36:38 +0200

    Updating circlei build

--- scripts/downloadDependencies.sh
+++ scripts/downloadDependencies.sh
@@ -5,7 +5,7 @@ source common.sh || source scripts/common.sh || echo "No common.sh script found.
 set -e
 
 ./mvnw -s .settings.xml --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/spring-cloud-contact-gradle-plugin; ./gradlew resolveDependencies || true
+cd $ROOT_FOLDER/spring-cloud-contract-tools/spring-cloud-contact-gradle-plugin; ./gradlew resolveDependencies || true
 cd $ROOT_FOLDER/samples/standalone/dsl/http-client; ./mvnw --fail-never dependency:go-offline || true
 cd $ROOT_FOLDER/samples/standalone/dsl/http-client; ./gradlew resolveDependencies || true
 cd $ROOT_FOLDER/samples/standalone/dsl/http-server; ./mvnw --fail-never dependency:go-offline || true
