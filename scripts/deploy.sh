82302ae721b59bde28e7e832a6b4503ae430fd00
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-08-12 10:46:36 +0200

    Fixed Contract build

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source common.sh || source scripts/common.sh || echo "No common.sh script found..."
-
-set -e
-
-./mvnw clean install && ./scripts/runTests.sh && ./mvnw deploy
--- scripts/downloadDependencies.sh
+++ scripts/downloadDependencies.sh
@@ -6,5 +6,15 @@ set -e
 
 ./mvnw -s .settings.xml --fail-never dependency:go-offline || true
 cd $ROOT_FOLDER/spring-cloud-contact-gradle-plugin && ./gradlew resolveDependencies || true
-cd $ROOT_FOLDER/samples/samples-standalone && ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/samples-standalone && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/dsl/http-client && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/dsl/http-client && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/dsl/http-server && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/dsl/http-server && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-client && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-client && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-server && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-server && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/messaging/stream-sink && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/messaging/stream-sink && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/messaging/sream-source && ./mvnw --fail-never dependency:go-offline || true
+cd $ROOT_FOLDER/samples/standalone/messaging/sream-source && ./downloadGradleDeps.sh
