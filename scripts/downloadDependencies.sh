74c846a27aaaf39491302c49c8702b0eb5de1c57
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-08-12 10:49:49 +0200

    Updating circlei build

--- scripts/downloadDependencies.sh
+++ scripts/downloadDependencies.sh
@@ -7,14 +7,14 @@ set -e
 ./mvnw -s .settings.xml --fail-never dependency:go-offline || true
 cd $ROOT_FOLDER/spring-cloud-contact-gradle-plugin && ./gradlew resolveDependencies || true
 cd $ROOT_FOLDER/samples/standalone/dsl/http-client && ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/dsl/http-client && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/dsl/http-client && ./gradlew resolveDependencies || true
 cd $ROOT_FOLDER/samples/standalone/dsl/http-server && ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/dsl/http-server && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/dsl/http-server && ./gradlew resolveDependencies || true
 cd $ROOT_FOLDER/samples/standalone/restdocs/http-client && ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/restdocs/http-client && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-client && ./gradlew resolveDependencies || true
 cd $ROOT_FOLDER/samples/standalone/restdocs/http-server && ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/restdocs/http-server && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/restdocs/http-server && ./gradlew resolveDependencies || true
 cd $ROOT_FOLDER/samples/standalone/messaging/stream-sink && ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/messaging/stream-sink && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/messaging/stream-sink && ./gradlew resolveDependencies || true
 cd $ROOT_FOLDER/samples/standalone/messaging/sream-source && ./mvnw --fail-never dependency:go-offline || true
-cd $ROOT_FOLDER/samples/standalone/messaging/sream-source && ./downloadGradleDeps.sh
+cd $ROOT_FOLDER/samples/standalone/messaging/sream-source && ./gradlew resolveDependencies || true
