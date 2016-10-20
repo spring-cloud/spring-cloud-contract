52b1785f4c67f5b5d17a573b557f13946ba4422d
Mathias Düsterhöft <mduesterhoeft@epages.com>
2016-10-20 09:27:13 +0200

    reference contract for docs from test

--- tests/spring-cloud-contract-stub-runner-amqp/src/test/groovy/org/springframework/cloud/contract/stubrunner/messaging/amqp/AmqpStubRunnerSpec.groovy
+++ tests/spring-cloud-contract-stub-runner-amqp/src/test/groovy/org/springframework/cloud/contract/stubrunner/messaging/amqp/AmqpStubRunnerSpec.groovy
@@ -5,6 +5,7 @@ import org.mockito.Captor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootContextLoader
 import org.springframework.boot.test.mock.mockito.SpyBean
+import org.springframework.cloud.contract.spec.Contract
 import org.springframework.cloud.contract.stubrunner.StubTrigger
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner
 import org.springframework.test.context.ContextConfiguration
@@ -26,6 +27,35 @@ class AmqpStubRunnerSpec extends Specification {
     ArgumentCaptor<Person> personArgumentCaptor
     
     def "should trigger stub amqp message"() {
+        given:
+        // tag::amqp_contract[]
+
+            Contract.make {
+                // Human readable description
+                description 'Should produce valid person data'
+                // Label by means of which the output message can be triggered
+                label 'contract-test.person.created.event'
+                // input to the contract
+                input {
+                    // the contract will be triggered by a method
+                    triggeredBy('createPerson()')
+                }
+                // output message of the contract
+                outputMessage {
+                    // destination to which the output message will be sent
+                    sentTo 'contract-test.exchange'
+                    headers {
+                        header('contentType': 'application/json')
+                        header('__TypeId__': 'org.springframework.cloud.contract.stubrunner.messaging.amqp.Person')
+                    }
+                    // the body of the output message
+                    body ([
+                            id: $(consumer(9), producer(regex("[0-9]+"))),
+                            name: "me"
+                    ])
+                }
+            }
+        // end::amqp_contract[]
         when:
         // tag::client_trigger[]
             stubTrigger.trigger("contract-test.person.created.event")
