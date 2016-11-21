6d4546966fb3dab555940ff573ff10a591511a6c
Andy Wilkinson <awilkinson@pivotal.io>
2016-11-21 16:11:16 +0000

    Use spring-asciidoctor-extensions to provide tabbed code blocks

--- scripts/generateDocs.sh
+++ scripts/generateDocs.sh
@@ -7,7 +7,7 @@ set -e
 echo "Generating docs for Spring Cloud Contract"
 
 echo "Building main docs"
-./mvnw clean install -P docs -DskipTests=true --pl docs-extensions,docs
+./mvnw clean install -P docs -DskipTests=true --pl docs
 
 echo "Building maven plugin docs"
 ./mvnw site -DskipTests=true --pl spring-cloud-contract-tools/spring-cloud-contract-maven-plugin
