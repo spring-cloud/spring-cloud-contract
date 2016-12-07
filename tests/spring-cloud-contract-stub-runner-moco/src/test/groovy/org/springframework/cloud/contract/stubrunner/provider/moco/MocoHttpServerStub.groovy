a85cdf0d022924efed7987aad041511a56acabf1
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-12-07 13:59:03 +0100

    Updated docs

--- tests/spring-cloud-contract-stub-runner-moco/src/test/groovy/org/springframework/cloud/contract/stubrunner/provider/moco/MocoHttpServerStub.groovy
+++ tests/spring-cloud-contract-stub-runner-moco/src/test/groovy/org/springframework/cloud/contract/stubrunner/provider/moco/MocoHttpServerStub.groovy
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2013-2016 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.springframework.cloud.contract.stubrunner.provider.moco
 
 import com.github.dreamhead.moco.bootstrap.arg.HttpArgs
@@ -5,9 +21,6 @@ import com.github.dreamhead.moco.runner.JsonRunner
 import org.springframework.cloud.contract.stubrunner.HttpServerStub
 import org.springframework.util.SocketUtils
 
-/**
- * @author Marcin Grzejszczak
- */
 class MocoHttpServerStub implements HttpServerStub {
 
 	private boolean started
