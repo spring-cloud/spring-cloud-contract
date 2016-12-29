4ceaff5b66f42ec9c29b24d9b9cb03305fee8d9e
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-12-29 15:38:34 +0100

    Added missing body pattern comparison
    without this change the eact contents of the body were mostly ignored
    with this change we support all WireMock body matching patterns
    
    fixes #135

--- spring-cloud-contract-wiremock/src/test/java/org/springframework/cloud/contract/wiremock/WiremockMockServerApplicationTests.java
+++ spring-cloud-contract-wiremock/src/test/java/org/springframework/cloud/contract/wiremock/WiremockMockServerApplicationTests.java
@@ -1,6 +1,8 @@
 package org.springframework.cloud.contract.wiremock;
 
 import java.net.URI;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
 import org.springframework.http.MediaType;
@@ -8,6 +10,8 @@ import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
@@ -215,6 +219,185 @@ public class WiremockMockServerApplicationTests {
 				.stubs("classpath:/io.stubs/mappings").build();
 		assertThat(this.restTemplate.getForObject("http://example.org/resource",
 				String.class)).isEqualTo("Hello World");
+	}
+
+	// Issue #135
+	@Test
+	public void postWithBodyMatchingJsonPaths() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-jsonpath.json").build();
+
+		assertThat(this.restTemplate.postForObject("http://example.org/body",
+				new Things(Collections.singletonList(new Thing("RequiredThing"))), String.class))
+				.isEqualTo("Hello Body");
+	}
+
+	@Test
+	public void postWithRequestThatHasNonMatchingJsonPaths() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-jsonpath.json").build();
+
+		String response;
+		try {
+			response = this.restTemplate.postForObject("http://example.org/body",
+					new Things(Collections.singletonList(new Thing("AbsentThing"))), String.class);
+		} catch (AssertionError e) {
+			response = null;
+		}
+		if (null != response) {
+			fail("There was a response for a request that shouldn't be matched : " + response);
+		}
+	}
+
+	@Test
+	public void postWithBodyMatchingXPath() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-xpath.json").build();
+
+		assertThat(this.restTemplate.exchange(
+				RequestEntity.post(URI.create("http://example.org/body"))
+					.contentType(MediaType.APPLICATION_XML)
+					.body("<things><thing><name>RequiredThing</name></thing></things>"),
+				String.class).getBody()).isEqualTo("Hello Body");
+	}
+
+	@Test
+	public void postWithRequestThatHasNonMatchingXPaths() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-xpath.json").build();
+
+		ResponseEntity<String> response;
+		try {
+			response = this.restTemplate.exchange(
+					RequestEntity.post(URI.create("http://example.org/body"))
+							.contentType(MediaType.APPLICATION_XML)
+							.body("<foo/>"),
+					String.class);
+		} catch (AssertionError e) {
+			response = null;
+		}
+		if (null != response) {
+			fail("There was a response for a request that shouldn't be matched : " + response);
+		}
+	}
+
+	@Test
+	public void postWithBodyMatchingJson() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-equaltojson.json").build();
+
+		assertThat(this.restTemplate.postForObject("http://example.org/body",
+				new Things(Collections.singletonList(new Thing("RequiredThing"))), String.class))
+				.isEqualTo("Hello Body");
+	}
+
+	@Test
+	public void postWithRequestThatHasNonMatchingJson() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-equaltojson.json").build();
+
+		String response;
+		try {
+			response = this.restTemplate.postForObject("http://example.org/body",
+					new Things(Collections.singletonList(new Thing("AbsentThing"))), String.class);
+		} catch (AssertionError e) {
+			response = null;
+		}
+		if (null != response) {
+			fail("There was a response for a request that shouldn't be matched : " + response);
+		}
+	}
+
+	@Test
+	public void postWithBodyMatchingXml() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-equaltoxml.json").build();
+
+		assertThat(this.restTemplate.exchange(
+				RequestEntity.post(URI.create("http://example.org/body"))
+						.contentType(MediaType.APPLICATION_XML)
+						.body("<things><thing><name>RequiredThing</name></thing></things>"),
+				String.class).getBody()).isEqualTo("Hello Body");
+	}
+
+	@Test
+	public void postWithRequestThatHasNonMatchingXml() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-equaltoxml.json").build();
+
+		ResponseEntity<String> response;
+		try {
+			response = this.restTemplate.exchange(
+					RequestEntity.post(URI.create("http://example.org/body"))
+							.contentType(MediaType.APPLICATION_XML)
+							.body("<things><thing><name>AbsentThing</name></thing></things>"),
+					String.class);
+		} catch (AssertionError e) {
+			response = null;
+		}
+		if (null != response) {
+			fail("There was a response for a request that shouldn't be matched : " + response);
+		}
+	}
+
+	@Test
+	public void postWithBodyMatchingRegex() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-regex.json").build();
+
+		assertThat(this.restTemplate.exchange(
+				RequestEntity.post(URI.create("http://example.org/body"))
+						.contentType(MediaType.APPLICATION_XML)
+						.body("<things><thing><name>RequiredThing</name></thing></things>"),
+				String.class).getBody()).isEqualTo("Hello Body");
+	}
+
+	@Test
+	public void postWithRequestThatHasNonMatchingRegex() throws Exception {
+		WireMockRestServiceServer.with(this.restTemplate) //
+				.baseUrl("http://example.org") //
+				.stubs("classpath:/mappings/body-matches-regex.json").build();
+
+		ResponseEntity<String> response;
+		try {
+			response = this.restTemplate.exchange(
+					RequestEntity.post(URI.create("http://example.org/body"))
+							.contentType(MediaType.APPLICATION_XML)
+							.body("<things><thing><name>AbsentThing</name></thing></things>"),
+					String.class);
+		} catch (AssertionError e) {
+			response = null;
+		}
+		if (null != response) {
+			fail("There was a response for a request that shouldn't be matched : " + response);
+		}
+	}
+
+	public static class Things {
+		public List<Thing> things;
+
+		@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+		public Things(List<Thing> things) {
+			this.things = things;
+		}
+	}
+
+	public static class Thing {
+		public String name;
+
+		@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+		public Thing(String name) {
+			this.name = name;
+		}
 	}
 
 }
