a06c1e93c266283fe2d96812371cea1048a02b1d
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-01-12 21:02:11 +0100

    Added two simple tests for ContentUtils
    the class is tested via JsonToJsonPathConverter

---
+++ spring-cloud-contract-spec/src/test/groovy/org/springframework/cloud/contract/spec/util/ContentUtilsSpec.groovy
@@ -0,0 +1,24 @@
+package org.springframework.cloud.contract.spec.util
+
+import org.springframework.cloud.contract.spec.internal.DslProperty
+import spock.lang.Specification
+
+/**
+ * @author Marcin Grzejszczak
+ */
+class ContentUtilsSpec extends Specification {
+
+	def "should return the stub side"() {
+		given:
+			DslProperty<String> dslProperty = new DslProperty<>("stub", "test")
+		expect:
+			"stub" == ContentUtils.GET_STUB_SIDE(dslProperty)
+	}
+
+	def "should return the test side"() {
+		given:
+			DslProperty<String> dslProperty = new DslProperty<>("stub", "test")
+		expect:
+			"test" == ContentUtils.GET_TEST_SIDE(dslProperty)
+	}
+}
