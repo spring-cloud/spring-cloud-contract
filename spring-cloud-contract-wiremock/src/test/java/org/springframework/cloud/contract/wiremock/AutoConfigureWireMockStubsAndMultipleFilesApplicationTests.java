6c14e8b2052b597f890d81bf297f9dd8e07094fb
Pei-Tang Huang <tangtheone@gmail.com>
2017-09-20 06:14:04 +0800

    Added composite child support. (#405)
    If specified subDirectoryName exists in more the one FileSource, return
    another instance of ResourcesFileSource containing all matched FileSources.
    
    * Fixed multiple files not being registered issue.
    * Use default FileSource if no valid resource specified.

--- spring-cloud-contract-wiremock/src/test/java/org/springframework/cloud/contract/wiremock/AutoConfigureWireMockStubsAndMultipleFilesApplicationTests.java
+++ spring-cloud-contract-wiremock/src/test/java/org/springframework/cloud/contract/wiremock/AutoConfigureWireMockStubsAndMultipleFilesApplicationTests.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=WiremockTestsApplication.class, properties="app.baseUrl=http://localhost:${wiremock.server.port}", webEnvironment=WebEnvironment.NONE)
 @DirtiesContext
-@AutoConfigureWireMock(port=0, files={"classpath:/nonexistent/", "classpath:/root/"}, stubs="file:src/test/resources/io.stubs/mappings")
+@AutoConfigureWireMock(port=0, files={"classpath:/", "classpath:/root/", "classpath:/nonexistent/"}, stubs="file:src/test/resources/io.stubs/mappings")
 public class AutoConfigureWireMockStubsAndMultipleFilesApplicationTests {
 
 	@Autowired
