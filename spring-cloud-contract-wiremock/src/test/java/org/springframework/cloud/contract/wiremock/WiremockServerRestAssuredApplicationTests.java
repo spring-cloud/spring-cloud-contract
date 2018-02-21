813460bbcf86751a4229ad18d59b68cb177223b4
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-02-21 13:49:20 +0100

    Fixed broken RestAssured 3.0, rest docs tests

--- spring-cloud-contract-wiremock/src/test/java/org/springframework/cloud/contract/wiremock/WiremockServerRestAssuredApplicationTests.java
+++ spring-cloud-contract-wiremock/src/test/java/org/springframework/cloud/contract/wiremock/WiremockServerRestAssuredApplicationTests.java
@@ -3,15 +3,15 @@ package org.springframework.cloud.contract.wiremock;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import wiremock.org.eclipse.jetty.http.HttpStatus;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import wiremock.org.eclipse.jetty.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -26,8 +27,7 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {ServletWebServerFactoryAutoConfiguration.EmbeddedTomcat.class, DispatcherServletAutoConfiguration.class},
-		webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureRestDocs(outputDir = "target/snippets")
 public class WiremockServerRestAssuredApplicationTests {
 
@@ -51,7 +51,7 @@ public class WiremockServerRestAssuredApplicationTests {
 
 	@Test
 	public void statusIsMaintained() throws Exception {
-		given(this.documentationSpec)
+		given(this.documentationSpec.port(this.port))
 				.filter(document("status"))
 		.when()
 				.get("/status")
@@ -64,6 +64,7 @@ public class WiremockServerRestAssuredApplicationTests {
 	@Configuration
 	@EnableWebMvc
 	@RestController
+	@ImportAutoConfiguration(DispatcherServletAutoConfiguration.class)
 	protected static class TestConfiguration {
 
 		@ResponseBody
@@ -76,6 +77,11 @@ public class WiremockServerRestAssuredApplicationTests {
 		@RequestMapping("/status")
 		public ResponseEntity<String> status() {
 			return ResponseEntity.status(HttpStatus.ACCEPTED_202).body("Hello World");
+		}
+
+		@Bean
+		TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+			return new TomcatServletWebServerFactory(0);
 		}
 
 	}
