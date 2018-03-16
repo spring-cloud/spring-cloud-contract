32888e3b92875655068f571fd2c17b70911c2831
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-03-16 16:04:36 +0100

    Add a convenience mechanism to get stub runner port
    Now, you can also use the `@StubRunnerPort` annotation to inject the port of a running stub.
    Value of the annotation can be the `groupid.artifactid` or just the `artifactid`. Example for Stub Runner ids
    `com.example:foo`, `com.example:bar`.
    
    ```java
    @StubRunnerPort("foo")
    int fooPort;
    @StubRunnerPort("com.example.bar")
    int barPort;
    ```
    
    fixes gh-573

--- spring-cloud-contract-stub-runner/src/main/java/org/springframework/cloud/contract/stubrunner/spring/StubRunnerConfiguration.java
+++ spring-cloud-contract-stub-runner/src/main/java/org/springframework/cloud/contract/stubrunner/spring/StubRunnerConfiguration.java
@@ -36,6 +36,7 @@ import org.springframework.cloud.contract.verifier.messaging.noop.NoOpStubMessag
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.io.Resource;
@@ -49,7 +50,7 @@ import org.springframework.core.io.Resource;
 @ConditionalOnMissingBean(type = "org.springframework.cloud.contract.wiremock.WiremockServerConfiguration")
 public class StubRunnerConfiguration {
 
-	private static final String STUBRUNNER_PREFIX = "stubrunner.runningstubs";
+	static final String STUBRUNNER_PREFIX = "stubrunner.runningstubs";
 
 	@Autowired(required = false)
 	private MessageVerifier<?> contractVerifierMessaging;
@@ -79,6 +80,11 @@ public class StubRunnerConfiguration {
 		RunningStubs runningStubs = batchStubRunner.runStubs();
 		registerPort(runningStubs);
 		return batchStubRunner;
+	}
+
+	@Bean
+	StubRunnerPortBeanPostProcessor stubRunnerPortBeanPostProcessor(Environment environment) {
+		return new StubRunnerPortBeanPostProcessor(environment);
 	}
 
 	private StubRunnerOptionsBuilder builder() throws IOException {
@@ -119,6 +125,8 @@ public class StubRunnerConfiguration {
 				.get(STUBRUNNER_PREFIX)).getSource();
 		for (Map.Entry<StubConfiguration, Integer> entry : runStubs.validNamesAndPorts().entrySet()) {
 			source.put(STUBRUNNER_PREFIX + "." + entry.getKey().getArtifactId() + ".port", entry.getValue());
+			// there are projects where artifact id is the same, what differs is the group id
+			source.put(STUBRUNNER_PREFIX + "." + entry.getKey().getGroupId() + "." + entry.getKey().getArtifactId() + ".port", entry.getValue());
 		}
 	}
 
---
+++ spring-cloud-contract-stub-runner/src/main/java/org/springframework/cloud/contract/stubrunner/spring/StubRunnerPortBeanPostProcessor.java
@@ -0,0 +1,61 @@
+package org.springframework.cloud.contract.stubrunner.spring;
+
+import java.lang.reflect.Field;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.env.Environment;
+import org.springframework.util.ReflectionUtils;
+
+/**
+ * Injects {@link StubRunnerPort} ports into fields
+ *
+ * @author Marcin Grzejszczak
+ * @since 2.0.0
+ */
+class StubRunnerPortBeanPostProcessor implements BeanPostProcessor {
+
+	private final Environment environment;
+
+	StubRunnerPortBeanPostProcessor(Environment environment) {
+		this.environment = environment;
+	}
+
+	@Override public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		injectStubRunnerPort(bean);
+		return bean;
+	}
+
+	private void injectStubRunnerPort(Object bean) {
+		Class<?> clazz = bean.getClass();
+		ReflectionUtils.FieldCallback fieldCallback =
+				new StubRunnerPortFieldCallback(this.environment, bean);
+		ReflectionUtils.doWithFields(clazz, fieldCallback);
+	}
+}
+
+class StubRunnerPortFieldCallback implements ReflectionUtils.FieldCallback {
+
+	private final Environment environment;
+	private final Object bean;
+
+	StubRunnerPortFieldCallback(Environment environment, Object bean) {
+		this.environment = environment;
+		this.bean = bean;
+	}
+
+	@Override public void doWith(Field field)
+			throws IllegalArgumentException, IllegalAccessException {
+		if (!field.isAnnotationPresent(StubRunnerPort.class)) {
+			return;
+		}
+		ReflectionUtils.makeAccessible(field);
+		String stub = field.getDeclaredAnnotation(StubRunnerPort.class).value();
+		Integer port = this.environment.getProperty(
+				StubRunnerConfiguration.STUBRUNNER_PREFIX + "." + stub + ".port", Integer.class);
+		if (port != null) {
+			field.set(this.bean, port);
+		}
+	}
+}
