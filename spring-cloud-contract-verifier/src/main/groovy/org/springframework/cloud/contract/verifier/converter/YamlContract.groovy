a4221327d7005e0677914d98be3a6d1ebd8501cb
Tim Ysewyn <Tim.Ysewyn@me.com>
2018-04-01 16:48:27 +0200

    Add/updated the ASF license header

--- spring-cloud-contract-verifier/src/main/groovy/org/springframework/cloud/contract/verifier/converter/YamlContract.groovy
+++ spring-cloud-contract-verifier/src/main/groovy/org/springframework/cloud/contract/verifier/converter/YamlContract.groovy
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2013-2016 the original author or authors.
+ *  Copyright 2013-2018 the original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import groovy.transform.CompileStatic
  * 
  * @since 1.2.1
  * @author Marcin Grzejszczak
+ * @author Tim Ysewyn
  */
 @CompileStatic
 class YamlContract {
--- spring-cloud-contract-verifier/src/test/groovy/org/springframework/cloud/contract/verifier/converter/YamlContractConverterSpec.groovy
+++ spring-cloud-contract-verifier/src/test/groovy/org/springframework/cloud/contract/verifier/converter/YamlContractConverterSpec.groovy
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2013-2016 the original author or authors.
+ *  Copyright 2013-2018 the original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -31,6 +31,7 @@ import org.springframework.cloud.contract.verifier.util.MapConverter
 
 /**
  * @author Marcin Grzejszczak
+ * @author Tim Ysewyn
  */
 class YamlContractConverterSpec extends Specification {
 
