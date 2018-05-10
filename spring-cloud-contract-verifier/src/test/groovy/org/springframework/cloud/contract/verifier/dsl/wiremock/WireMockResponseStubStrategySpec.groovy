3fe82362a8276ba6f1047ea4a86b82aceb1814df
Tim Ysewyn <TYsewyn@users.noreply.github.com>
2018-05-11 01:54:20 +0200

    Return the actual object instead of the String used for matching the pattern (#654)

--- spring-cloud-contract-verifier/src/test/groovy/org/springframework/cloud/contract/verifier/dsl/wiremock/WireMockResponseStubStrategySpec.groovy
+++ spring-cloud-contract-verifier/src/test/groovy/org/springframework/cloud/contract/verifier/dsl/wiremock/WireMockResponseStubStrategySpec.groovy
@@ -1,9 +1,12 @@
 package org.springframework.cloud.contract.verifier.dsl.wiremock
 
+import groovy.json.JsonSlurper
 import org.springframework.cloud.contract.spec.Contract
+import spock.lang.Issue
 import spock.lang.Specification
 
 class WireMockResponseStubStrategySpec extends Specification {
+
 	def "should not quote floating point numbers"() {
 		given:
 			def irrelevantStatus = 200
@@ -22,5 +25,33 @@ class WireMockResponseStubStrategySpec extends Specification {
 			def content = subject.buildClientResponseContent()
 		then:
 			'{"value":1.5}'.equals(content.body)
+	}
+
+	@Issue("#468")
+	def "should not quote generated numbers"() {
+		given:
+			def irrelevantStatus = 200
+			def contract = Contract.make {
+				request {
+				}
+				response {
+					status irrelevantStatus
+					body([
+						number: anyNumber(),
+						integer: anyInteger(),
+						positiveInt: anyPositiveInt(),
+						double: anyDouble(),
+					])
+				}
+			}
+		when:
+			def subject = new WireMockResponseStubStrategy(contract)
+			def content = subject.buildClientResponseContent()
+		then:
+			Map body = new JsonSlurper().parseText(content.body) as Map
+			assert body.get("number") instanceof Number
+			assert body.get("integer") instanceof Integer
+			assert body.get("positiveInt") instanceof Integer
+			assert body.get("double") instanceof BigDecimal
 	}
 }
