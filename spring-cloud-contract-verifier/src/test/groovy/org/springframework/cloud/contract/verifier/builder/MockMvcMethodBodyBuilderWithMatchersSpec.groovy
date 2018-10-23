2d5c2dd9312f75fa2c4a5a862f1d680a6885e0b3
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-10-23 22:53:23 +0200

    Fixes missing casting; fixes gh-764

--- spring-cloud-contract-verifier/src/test/groovy/org/springframework/cloud/contract/verifier/builder/MockMvcMethodBodyBuilderWithMatchersSpec.groovy
+++ spring-cloud-contract-verifier/src/test/groovy/org/springframework/cloud/contract/verifier/builder/MockMvcMethodBodyBuilderWithMatchersSpec.groovy
@@ -189,7 +189,7 @@ class MockMvcMethodBodyBuilderWithMatchersSpec extends Specification implements 
 			test.contains('assertThat((java.lang.Iterable) parsedJson.read("' + rootElement + '.valueWithMaxEmpty", java.util.Collection.class)).as("' + rootElement + '.valueWithMaxEmpty").hasSizeLessThanOrEqualTo(0)')
 			test.contains('assertThatValueIsANumber(parsedJson.read("' + rootElement + '.duck")')
 			test.contains('assertThat(parsedJson.read("' + rootElement + '''.['key'].['complex.key']", String.class)).isEqualTo("foo")''')
-			test.contains('assertThat(parsedJson.read("' + rootElement + '.nullValue")).isNull()')
+			test.contains('assertThat((Object) parsedJson.read("' + rootElement + '.nullValue")).isNull()')
 			!test.contains('cursor')
 		and:
 			try {
