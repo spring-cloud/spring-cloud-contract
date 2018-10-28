14d968a63f5159fd9c69e7eb9e46f945fbc8ac4d
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-10-28 14:07:28 +0100

    Should pick all the properties

--- spring-cloud-contract-tools/spring-cloud-contract-gradle-plugin/src/main/groovy/org/springframework/cloud/contract/verifier/plugin/ExtensionToProperties.groovy
+++ spring-cloud-contract-tools/spring-cloud-contract-gradle-plugin/src/main/groovy/org/springframework/cloud/contract/verifier/plugin/ExtensionToProperties.groovy
@@ -13,24 +13,24 @@ class ExtensionToProperties {
 
 	protected static ContractVerifierConfigProperties fromExtension(ContractVerifierExtension extension) {
 		return new ContractVerifierConfigProperties(
-				targetFramework: extension.targetFramework,
-				testMode: extension.testMode,
-				basePackageForTests: extension.basePackageForTests,
-				baseClassForTests: extension.baseClassForTests,
-				nameSuffixForTests: extension.nameSuffixForTests,
-				ruleClassForTests: extension.ruleClassForTests,
-				excludedFiles: extension.excludedFiles,
-				ignoredFiles: extension.ignoredFiles,
-				imports: extension.imports,
-				staticImports: extension.staticImports,
-				contractsDslDir: extension.contractsDslDir,
-				generatedTestSourcesDir: extension.generatedTestSourcesDir,
-				stubsOutputDir: extension.stubsOutputDir,
-				stubsSuffix: extension.stubsSuffix,
-				assertJsonSize: extension.assertJsonSize,
-				packageWithBaseClasses: extension.packageWithBaseClasses,
-				baseClassMappings: extension.baseClassMappings,
-				excludeBuildFolders: extension.excludeBuildFolders
+				targetFramework: extension.getTargetFramework(),
+				testMode: extension.getTestMode(),
+				basePackageForTests: extension.getBasePackageForTests(),
+				baseClassForTests: extension.getBaseClassForTests(),
+				nameSuffixForTests: extension.getNameSuffixForTests(),
+				ruleClassForTests: extension.getRuleClassForTests(),
+				excludedFiles: extension.getExcludedFiles(),
+				ignoredFiles: extension.getIgnoredFiles(),
+				imports: extension.getImports(),
+				staticImports: extension.getStaticImports(),
+				contractsDslDir: extension.getContractsDslDir(),
+				generatedTestSourcesDir: extension.getGeneratedTestSourcesDir(),
+				stubsOutputDir: extension.getStubsOutputDir(),
+				stubsSuffix: extension.getStubsSuffix(),
+				assertJsonSize: extension.getAssertJsonSize(),
+				packageWithBaseClasses: extension.getPackageWithBaseClasses(),
+				baseClassMappings: extension.getBaseClassMappings(),
+				excludeBuildFolders: extension.getExcludeBuildFolders()
 		)
 	}
 }
