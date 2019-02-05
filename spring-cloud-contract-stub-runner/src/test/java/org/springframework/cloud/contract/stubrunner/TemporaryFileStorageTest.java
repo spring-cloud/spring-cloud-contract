ec0dd872acac6d7fc508f76d83daa6178cf32bc6
konstantin <konstantin.shevchuk@zamro.com>
2019-02-05 21:53:40 +0100

    Increase test coverage.

--- spring-cloud-contract-stub-runner/src/test/java/org/springframework/cloud/contract/stubrunner/TemporaryFileStorageTest.java
+++ spring-cloud-contract-stub-runner/src/test/java/org/springframework/cloud/contract/stubrunner/TemporaryFileStorageTest.java
@@ -5,20 +5,29 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.Assert.assertFalse;
 
 public class TemporaryFileStorageTest {
+
     @Test
-    public void should_cleanup_temporary_file_storage() throws IOException {
+    public void should_delete_file_inside_directory_from_temporary_file_storage() throws IOException {
         //given:
-        File dummyContractFile = Files.createTempFile("dummy-contract", ".groovy").toFile();
+        Path tmpContractDirectory = Files.createTempDirectory("tmp");
+        File tmpContractFile = new File(tmpContractDirectory.toString());
+        File dummyContractFileInsideDirectory = Files.createTempFile(tmpContractDirectory, "dummy-contract-inside-folder", ".groovy").toFile();
+        File dummyContractFile = Files.createTempFile( "dummy-contract", ".groovy").toFile();
         TemporaryFileStorage.add(dummyContractFile);
+        TemporaryFileStorage.add(tmpContractFile);
+
 
         //when:
         TemporaryFileStorage.cleanup(true);
 
         //then:
         assertFalse(dummyContractFile.exists());
+        assertFalse(dummyContractFileInsideDirectory.exists());
+        assertFalse(tmpContractDirectory.toFile().exists());
     }
 }
