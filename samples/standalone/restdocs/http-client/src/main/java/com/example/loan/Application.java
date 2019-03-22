a607ccf376c3b32caa6def5f9c89f5277a79c28f
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-03-22 15:08:40 +0100

    Fixed

--- samples/standalone/restdocs/http-client/src/main/java/com/example/loan/Application.java
+++ samples/standalone/restdocs/http-client/src/main/java/com/example/loan/Application.java
@@ -18,8 +18,10 @@ package com.example.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ServiceConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
