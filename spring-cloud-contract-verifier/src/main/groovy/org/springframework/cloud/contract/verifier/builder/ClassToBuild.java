86a7bd77c9011b9f8460f485ff5fa1e0c77f4723
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-05-09 09:03:35 +0200

    WIP

--- spring-cloud-contract-verifier/src/main/groovy/org/springframework/cloud/contract/verifier/builder/ClassToBuild.java
+++ spring-cloud-contract-verifier/src/main/groovy/org/springframework/cloud/contract/verifier/builder/ClassToBuild.java
@@ -2,6 +2,7 @@ package org.springframework.cloud.contract.verifier.builder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ClassToBuild {
 	private ClassMetaData metaData;
