9ab4a83cd862c348ca65cf7cbb2a0954a752cc8c
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2019-06-28 11:27:57 +0200

    Fix failing test. Remove unused assignment.

--- spring-cloud-contract-verifier/src/test/groovy/org/springframework/cloud/contract/verifier/builder/JaxRsClientMethodBuilderSpec.groovy
+++ spring-cloud-contract-verifier/src/test/groovy/org/springframework/cloud/contract/verifier/builder/JaxRsClientMethodBuilderSpec.groovy
@@ -918,8 +918,6 @@ class JaxRsClientMethodBuilderSpec extends Specification implements WireMockStub
 					"""\
 package com.example;
 
-import org.junit.FixMethodOrder;
-import org.junit.runners.MethodSorters;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.Test;
@@ -1289,11 +1287,6 @@ public class FooTest {
 			!test.contains('REGEXP>>')
 		and:
 			SyntaxChecker.tryToCompile(methodBuilderName, test)
-		and:
-			String jsonSample = '''\
-	String json = "{\\"duck\\":\\"8\\",\\"alpha\\":\\"YAJEOWYGMFBEWPMEMAZI\\",\\"number\\":-2095030871,\\"positiveInt\\":42,\\"aDouble\\":42.345,\\"aBoolean\\":true,\\"ip\\":\\"129.168.99.100\\",\\"hostname\\":\\"https://foo389886219.com\\",\\"email\\":\\"foo@bar1367573183.com\\",\\"url\\":\\"https://foo-597104692.com\\",\\"httpsUrl\\":\\"https://baz-486093581.com\\",\\"uuid\\":\\"e436b817-b764-49a2-908e-967f2f99eb9f\\",\\"date\\":\\"2014-04-14\\",\\"dateTime\\":\\"2011-01-11T12:23:34\\",\\"time\\":\\"12:20:30\\",\\"iso8601WithOffset\\":\\"2015-05-15T12:23:34.123Z\\",\\"nonBlankString\\":\\"EPZWVIRHSUAPBJMMQSFO\\",\\"nonEmptyString\\":\\"RVMFDSEQFHRQFVUVQPIA\\",\\"anyOf\\":\\"foo\\"}";
-	DocumentContext parsedJson = JsonPath.parse(json);
-	'''
 		where:
 			methodBuilderName | methodBuilder | endOfLineRegexSymbol
 			"jaxrs-spock"     | {
