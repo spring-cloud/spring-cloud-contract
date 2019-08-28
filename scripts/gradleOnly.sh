cdb93f178b875e1118a58446ec16380affe7fb61
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-08-28 10:50:26 +0200

    Trying to fix circle

---
+++ scripts/gradleOnly.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source common.sh || source scripts/common.sh || echo "No common.sh script found..."
+
+FOLDER=`pwd`
+
+set -e
+
+cd "${FOLDER}/spring-cloud-contract-tools/spring-cloud-contract-gradle-plugin"
+  ./gradlew clean build install
+cd "${FOLDER}"
