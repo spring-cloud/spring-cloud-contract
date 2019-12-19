454e5b45a8177ba80e68114a27989fa2d5d5b844
Dominik Labuda <dominikla@gmail.com>
2019-12-19 01:56:52 +0100

    Local stub publishing reimplemented (#1291)
    While the previous approach did publish the contracts to local .m2 repository, it did not create the maven-metadata-local.xml file indicating a local release. It instead created maven-metadata.xml, which indicates a remote release. This wasn't issue when the stubs were retrieved by Gradle directly (in our flow we pre-check availability of these stubs by Gradle), it did however cause problems when they were being resolved by AetherStubDownloader used in SCC Stub Runner.
    
    In addition, this newer implementation allows to enable any publish (remote or local) independently of each other, which wasn't the case previously.
    
    Fixes #1283 - again

--- docker/spring-cloud-contract-docker/project/build.sh
+++ docker/spring-cloud-contract-docker/project/build.sh
@@ -4,4 +4,4 @@ export PROJECT_NAME="${PROJECT_NAME:-example}"
 echo "Setting project name to [${PROJECT_NAME}]"
 echo "rootProject.name='${PROJECT_NAME}'" >> settings.gradle
 echo "Running the build"
-./gradlew clean build publish --stacktrace
+./gradlew clean build publishToMavenLocal publish --stacktrace
