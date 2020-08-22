9a0a5c59d0c97b792f4c88d588514878f2757474
santiago <jomolanoper@gmail.com>
2020-08-22 14:08:02 -0500

    Convert from Groovy to Java

---
+++ spring-cloud-contract-verifier/src/main/groovy/org/springframework/cloud/contract/verifier/dsl/wiremock/DefaultResponseTransformer.java
@@ -0,0 +1,42 @@
+package org.springframework.cloud.contract.verifier.dsl.wiremock;
+
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+import org.springframework.cloud.contract.verifier.builder.handlebars.HandlebarsEscapeHelper;
+import org.springframework.cloud.contract.verifier.builder.handlebars.HandlebarsJsonPathHelper;
+import wiremock.com.github.jknack.handlebars.Helper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Default implementation of {@link ResponseTemplateTransformer} that contains
+ * default set of handlebars helpers
+ *
+ * @author Marcin Grzejszczak
+ * @since 1.2.0
+ */
+public class DefaultResponseTransformer extends ResponseTemplateTransformer {
+	public DefaultResponseTransformer() {
+		super(false, defaultHelpers());
+	}
+
+	public DefaultResponseTransformer(boolean global) {
+		super(global);
+	}
+
+	public DefaultResponseTransformer(boolean global, String helperName, Helper helper) {
+		super(global, helperName, helper);
+	}
+
+	public DefaultResponseTransformer(boolean global, Map<String, Helper> helpers) {
+		super(global, helpers);
+	}
+
+	private static Map<String, Helper> defaultHelpers() {
+		Map<String, Helper> helpers = new HashMap<String, Helper>();
+		helpers.put(HandlebarsJsonPathHelper.NAME, new HandlebarsJsonPathHelper());
+		helpers.put(HandlebarsEscapeHelper.NAME, new HandlebarsEscapeHelper());
+		return helpers;
+	}
+
+}
