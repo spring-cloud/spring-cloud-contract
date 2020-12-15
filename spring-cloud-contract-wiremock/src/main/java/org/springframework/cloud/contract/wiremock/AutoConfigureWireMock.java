8dde26ddff7aa4b7e1ecbc5369eae6e12bef9cd6
Sudhir Tumati <mailofsudhir@gmail.com>
2020-12-15 12:24:29 +0800

    Add javadoc for AutoConfigureWireMock

--- spring-cloud-contract-wiremock/src/main/java/org/springframework/cloud/contract/wiremock/AutoConfigureWireMock.java
+++ spring-cloud-contract-wiremock/src/main/java/org/springframework/cloud/contract/wiremock/AutoConfigureWireMock.java
@@ -46,12 +46,50 @@ import org.springframework.context.annotation.Import;
 @Inherited
 public @interface AutoConfigureWireMock {
 
+	/**
+	 * Configures WireMock instance to listen on specified port.
+	 * <p>
+	 * Set this value to 0 for WireMock to listen to a random port.
+	 * </p>
+	 * @return port to which WireMock instance should listen to
+	 */
 	int port() default 8080;
 
+	/**
+	 * If specified, configures WireMock instance to enable <em>HTTPS</em> on specified
+	 * port.
+	 * <p>
+	 * Set this value to 0 for WireMock to listen to a random port.
+	 * </p>
+	 * @return port to which WireMock instance should listen to
+	 */
 	int httpsPort() default -1;
 
+	/**
+	 * The resource locations to use for loading WireMock mappings.
+	 * <p>
+	 * When none specified, <em>src/test/resources/mappings</em> is used as default
+	 * location.
+	 * </p>
+	 * <p>
+	 * To customize the location, this attribute must be set to the directory where
+	 * mappings are stored.
+	 * </p>
+	 * @return locations to read WireMock mappings from
+	 */
 	String[] stubs() default { "" };
 
+	/**
+	 * The resource locations to use for loading WireMock response bodies.
+	 * <p>
+	 * When none specified, <em>src/test/resources/__files</em> is used as default.
+	 * </p>
+	 * <p>
+	 * To customize the location, this attribute must be set to the parent directory of
+	 * <strong>__files</strong> directory.
+	 * </p>
+	 * @return locations to read WireMock response bodies from
+	 */
 	String[] files() default { "" };
 
 }
