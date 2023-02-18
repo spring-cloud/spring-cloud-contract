76451e4ba3f31d1512672d04b858c228be6dc869
Václav Plic <plic.vasek@gmail.com>
2023-02-18 17:08:11 +0100

    Move PluginUnitTest into integration-test phase

--- spring-cloud-contract-tools/spring-cloud-contract-maven-plugin/src/test/java/org/springframework/cloud/contract/maven/verifier/PluginUnitTest.java
+++ spring-cloud-contract-tools/spring-cloud-contract-maven-plugin/src/test/java/org/springframework/cloud/contract/maven/verifier/PluginUnitTest.java
@@ -19,7 +19,6 @@ package org.springframework.cloud.contract.maven.verifier;
 import java.io.File;
 
 import org.codehaus.plexus.util.xml.Xpp3Dom;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static java.nio.charset.Charset.defaultCharset;
@@ -27,8 +26,6 @@ import static org.apache.commons.io.FileUtils.readFileToString;
 import static org.apache.maven.plugin.testing.MojoParameters.newParameter;
 import static org.assertj.core.api.BDDAssertions.then;
 
-// TODO: Move this to integration tests
-@Ignore("Testing harness uses an old version of maven")
 public class PluginUnitTest extends AbstractMojoTest {
 
 	@Test
