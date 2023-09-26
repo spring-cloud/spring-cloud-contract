0ee72919f2fadae341f0cce5abf3efc664ac5a0b
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2023-09-26 12:51:30 +0200

    Not building the maven plugin docs anymore

--- scripts/generateDocs.sh
+++ scripts/generateDocs.sh
@@ -8,9 +8,3 @@ echo "Generating docs for Spring Cloud Contract"
 
 echo "Building main docs"
 ./mvnw clean install -P docs --pl docs ${@}
-
-echo "Building maven plugin docs"
-./mvnw site -DskipTests=true --pl spring-cloud-contract-tools/spring-cloud-contract-maven-plugin ${@}
-
-echo "Copying generated maven plugin docs to main docs"
-cp -r spring-cloud-contract-tools/spring-cloud-contract-maven-plugin/target/site docs/target/generated-docs/spring-cloud-contract-maven-plugin
