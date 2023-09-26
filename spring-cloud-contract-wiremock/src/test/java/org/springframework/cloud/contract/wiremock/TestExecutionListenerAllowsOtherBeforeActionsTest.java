2e6550f9d124a1a536d518fd0866d76db9becdf9
Paulius Paplauskas <paulius.papl@gmail.com>
2023-09-26 15:58:02 +0300

    code style changes

--- spring-cloud-contract-wiremock/src/test/java/org/springframework/cloud/contract/wiremock/TestExecutionListenerAllowsOtherBeforeActionsTest.java
+++ spring-cloud-contract-wiremock/src/test/java/org/springframework/cloud/contract/wiremock/TestExecutionListenerAllowsOtherBeforeActionsTest.java
@@ -16,31 +16,32 @@
 
 package org.springframework.cloud.contract.wiremock;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.test.annotation.DirtiesContext;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.cloud.contract.wiremock.TestExecutionListenerAllowsOtherBeforeActionsTest.ApplicationEventListener;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.test.annotation.DirtiesContext;
-
 @SpringBootTest(classes = WiremockTestsApplication.class,
 		properties = "app.baseUrl=http://localhost:${wiremock.server.port}", webEnvironment = WebEnvironment.NONE)
 @AutoConfigureWireMock(port = 0)
 @DirtiesContext
-@Import(ApplicationEventListener.class)
+@Import(TestExecutionListenerAllowsOtherBeforeActionsTest.ApplicationEventListener.class)
 class TestExecutionListenerAllowsOtherBeforeActionsTest {
 
 	private static boolean beforeAllRunSuccessful;
+
 	private static boolean applicationContextInitialized;
 
 	@Autowired
@@ -53,7 +54,8 @@ class TestExecutionListenerAllowsOtherBeforeActionsTest {
 
 	@Test
 	void should_run_before_all_action_before_application_context_is_available() {
-		// check that beforeAll method was actually run before the application context was initialized
+		// check that beforeAll method was actually run before the application context was
+		// initialized
 		assertThat(beforeAllRunSuccessful).isTrue();
 		// check that wiremock instance is running
 		stubFor(get(urlEqualTo("/test"))
@@ -62,9 +64,12 @@ class TestExecutionListenerAllowsOtherBeforeActionsTest {
 	}
 
 	public static class ApplicationEventListener implements ApplicationListener<ContextRefreshedEvent> {
+
 		@Override
 		public void onApplicationEvent(ContextRefreshedEvent event) {
 			applicationContextInitialized = true;
 		}
+
 	}
+
 }
