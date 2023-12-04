981be7859a6da345b8173869a0cac7e9e8bbfa50
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2023-12-04 15:55:06 +0100

    Fixed checkstye

--- spring-cloud-contract-wiremock/src/main/java/org/springframework/cloud/contract/wiremock/WireMockApplicationListener.java
+++ spring-cloud-contract-wiremock/src/main/java/org/springframework/cloud/contract/wiremock/WireMockApplicationListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ package org.springframework.cloud.contract.wiremock;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import java.util.Optional;
 
 import org.apache.commons.logging.Log;
