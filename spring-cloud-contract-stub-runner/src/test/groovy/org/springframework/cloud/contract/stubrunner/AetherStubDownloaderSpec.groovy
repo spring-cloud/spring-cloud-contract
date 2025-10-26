1c5f24ee7d2c804999bfbd883d2947aa06442dac
Ryan Baxter <ryan.baxter@broadcom.com>
2025-10-26 17:02:29 -0400

    Remove hoverfly

--- spring-cloud-contract-stub-runner/src/test/groovy/org/springframework/cloud/contract/stubrunner/AetherStubDownloaderSpec.groovy
+++ spring-cloud-contract-stub-runner/src/test/groovy/org/springframework/cloud/contract/stubrunner/AetherStubDownloaderSpec.groovy
@@ -16,10 +16,8 @@
 
 package org.springframework.cloud.contract.stubrunner
 
-import io.specto.hoverfly.junit.HoverflyRule
 import org.eclipse.aether.RepositorySystemSession
 import org.eclipse.aether.repository.Authentication
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
@@ -29,9 +27,6 @@ import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
 import org.springframework.util.ResourceUtils
 
 class AetherStubDownloaderSpec extends Specification {
-
-	@Rule
-	HoverflyRule hoverflyRule = HoverflyRule.inSimulationMode("simulation.json")
 
 	@Rule
 	TemporaryFolder folder = new TemporaryFolder()
