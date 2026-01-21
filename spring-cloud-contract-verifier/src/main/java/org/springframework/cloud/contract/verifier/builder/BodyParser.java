730e29f83ab5fd58dbd91eae472a99851e71aee5
Marcin Grzejszczak <marcin@grzejszczak.pl>
2026-01-21 16:34:53 +0100

    fix: potential fix for CI problems
    Signed-off-by: Marcin Grzejszczak <marcin@grzejszczak.pl>

--- spring-cloud-contract-verifier/src/main/java/org/springframework/cloud/contract/verifier/builder/BodyParser.java
+++ spring-cloud-contract-verifier/src/main/java/org/springframework/cloud/contract/verifier/builder/BodyParser.java
@@ -136,7 +136,8 @@ interface BodyParser extends BodyThen {
 	}
 
 	default String escape(String text) {
-		return StringEscapeUtils.escapeJava(text);
+		String escaped = StringEscapeUtils.escapeJava(text);
+		return escaped.replace("\r", "\\r").replace("\n", "\\n");
 	}
 
 	default String escapeForSimpleTextAssertion(String text) {
